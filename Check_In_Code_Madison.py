b6bc1ea61883065acb1a52b2aae254940d92ddb5
mlm4612 <mmedley1@terpmail.umd.edu>
2021-11-11 22:11:53 -0500

    This is Madison's portion of the Check In

---
+++ Check_In_Code_Madison.py
@@ -0,0 +1,33 @@
+import random
+
+def turn(row, col):
+    """Allows the player to take a turn
+        
+        Arguments:
+        - row (int) - the current row that the player is in
+        - row (int) - the current row that the player is in
+        
+        Returns:
+        - (row, col) (tuple) - the new coordinates of the player
+        
+        """
+    steps = random.randint(1,6)
+    direction = ["left", "right", "up", "down"]
+    turn_direction = random.choice(direction)
+    if turn_direction == "left":
+        col -= steps
+    elif turn_direction == "right":
+        col += steps
+    elif turn_direction == "up":
+        row += steps
+    elif turn_direction == "down":
+        row -= steps
+
+    if col <= 0:
+        col = 0
+
+    if row <= 0:
+        row = 0
+    return (row, col)
+
+print(turn(1, 2))
